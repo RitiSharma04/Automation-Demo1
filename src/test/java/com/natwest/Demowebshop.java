b4d9774ac33c2e55af35a24011c5d39530518917
Riti Sharma <sharrfv@rbos.co.uk>
2024-03-20 22:41:17 -0700

    Initial commit

--- src/test/java/com/natwest/Demowebshop.java
+++ src/test/java/com/natwest/Demowebshop.java
@@ -12,22 +12,22 @@ public class Demowebshop {
         WebDriver driver=new ChromeDriver();
         driver.get("https://demowebshop.tricentis.com/register");
         driver.manage().window().maximize();
-//        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
-//
-//        driver.findElement(By.xpath("//input[@id='gender-male']")).click();
-//        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Mayank");
-//        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Bhardwaj");
-//        driver.findElement(By.xpath("//input[@name='Email']")).sendKeys("mayank23@namvgurukul.org");
-//        driver.findElement(By.xpath("//input[@name='Password']")).sendKeys("Riti@76546");
-//        driver.findElement(By.xpath("//input[@name='ConfirmPassword']")).sendKeys("Riti@76546");
-//        driver.findElement(By.xpath("//input[@name='register-button']")).click();
-//        driver.findElement(By.xpath("//input[@class='button-1 register-continue-button']")).click();
-//        driver.findElement(By.xpath("//a[@class='ico-logout']")).click();
-//        driver.findElement(By.xpath("//a[@class='ico-login']")).click();
-//        driver.findElement(By.xpath("//input[@name='Email']")).sendKeys("mayank23@namvgurukul.org");
-//        driver.findElement(By.xpath("//input[@name='Password']")).sendKeys("Riti@76546");
-//        driver.findElement(By.xpath("//input[@id='RememberMe']")).click();
-//        driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
-//        driver.findElement(By.xpath("//a[@class='ico-logout']")).click();
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
+
+        driver.findElement(By.xpath("//input[@id='gender-male']")).click();
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Mayank");
+        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Bhardwaj");
+        driver.findElement(By.xpath("//input[@name='Email']")).sendKeys("mayank23@namvgurukul.org");
+        driver.findElement(By.xpath("//input[@name='Password']")).sendKeys("Riti@76546");
+        driver.findElement(By.xpath("//input[@name='ConfirmPassword']")).sendKeys("Riti@76546");
+        driver.findElement(By.xpath("//input[@name='register-button']")).click();
+        driver.findElement(By.xpath("//input[@class='button-1 register-continue-button']")).click();
+        driver.findElement(By.xpath("//a[@class='ico-logout']")).click();
+        driver.findElement(By.xpath("//a[@class='ico-login']")).click();
+        driver.findElement(By.xpath("//input[@name='Email']")).sendKeys("mayank23@namvgurukul.org");
+        driver.findElement(By.xpath("//input[@name='Password']")).sendKeys("Riti@76546");
+        driver.findElement(By.xpath("//input[@id='RememberMe']")).click();
+        driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
+        driver.findElement(By.xpath("//a[@class='ico-logout']")).click();
     }
 }
